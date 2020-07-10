69258a75373c9c0642df5289ad69334d416e0967
zahego <tug70814@temple.edu>
2020-07-09 21:11:27 -0400

    hmm?

---
+++ build/classes/common/Board/Board.rs
@@ -0,0 +1,8 @@
+common.Board.SortSummaryAsc
+common.Board.SortDueDate
+common.Board.SortSummaryDesc
+common.Board.Board
+common.Board.SortSeverityAsc
+common.Board.SortIDAsc
+common.Board.SortSeverityDesc
+common.Board.SortIDDesc
